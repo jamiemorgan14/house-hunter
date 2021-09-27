631d044106b25d97bbfacfc59c8a7f6ed26c60f1
Jamie <jmorgan3475@gmail.com>
2021-09-27 17:55:31 -0600

    init

---
+++ src/main-page/index.js
@@ -0,0 +1,41 @@
+import { useEffect, useState, useMemo } from "react";
+import './main-page.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from "./header";
+import FeaturedHouse from "./featured-house";
+
+function App() {
+  const [allHouses, setAllHouses] = useState([]);
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch('/houses.json');
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
+  return (
+    <Router>
+      <div className="container">
+        <Header
+          subtitle="Providing houses all over the world"
+        />
+        <Switch>
+          <Route path="/">
+            <FeaturedHouse house={featuredHouse} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
