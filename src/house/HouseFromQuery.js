c13b0f40e54c141f216db6c491200b95d6b98cdd
Jamie <jmorgan3475@gmail.com>
2021-09-27 19:18:29 -0600

    custom hooks and context

--- src/house/HouseFromQuery.js
+++ src/house/HouseFromQuery.js
@@ -1,8 +1,11 @@
 import { useParams } from "react-router-dom";
+import { useContext } from "react/cjs/react.development";
 import House from ".";
+import HousesContext from "../context/housesContext";
 
-const HouseFromQuery = ({ allHouses }) => {
+const HouseFromQuery = () => {
   const { id } = useParams();
+  const allHouses = useContext(HousesContext);
   const house = allHouses.find(h => h.id === parseInt(id));
   return <House house={house} />;
 }
--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState, useMemo } from "react";
 import './main-page.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from "./header";
@@ -6,44 +5,34 @@ import FeaturedHouse from "./featured-house";
 import SearchResults from '../search-results/index';
 import HouseFilter from "./house-filter";
 import HouseFromQuery from "../house/HouseFromQuery";
+import useHouses from "../hooks/useHouses";
+import useFeaturedHouse from "../hooks/useFeaturedHouse";
+import HousesContext from '../context/housesContext';
 
 function App() {
-  const [allHouses, setAllHouses] = useState([]);
-  useEffect(() => {
-    const fetchHouses = async () => {
-      const rsp = await fetch('/houses.json');
-      const houses = await rsp.json();
-      setAllHouses(houses);
-    };
-    fetchHouses();
-  }, []);
-
-  const featuredHouse = useMemo(() => {
-    if (allHouses.length) {
-      const randomIndex = Math.floor(Math.random() * allHouses.length);
-      return allHouses[randomIndex];
-    }
-  }, [allHouses]);
-
+  const allHouses = useHouses();
+  const featuredHouse = useFeaturedHouse(allHouses);
   return (
     <Router>
-      <div className="container">
-        <Header
-          subtitle="Providing houses all over the world"
-        />
-        <HouseFilter allHouses={allHouses} />
-        <Switch>
-          <Route path="/searchresults/:country">
-            <SearchResults allHouses={allHouses} />
-          </Route>
-          <Route path="/house/:id">
-            <HouseFromQuery allHouses={allHouses} />
-          </Route>
-          <Route path="/">
-            <FeaturedHouse house={featuredHouse} />
-          </Route>
-        </Switch>
-      </div>
+      <HousesContext.Provider value={allHouses}>
+        <div className="container">
+          <Header
+            subtitle="Providing houses all over the world"
+          />
+          <HouseFilter />
+          <Switch>
+            <Route path="/searchresults/:country">
+              <SearchResults />
+            </Route>
+            <Route path="/house/:id">
+              <HouseFromQuery />
+            </Route>
+            <Route path="/">
+              <FeaturedHouse house={featuredHouse} />
+            </Route>
+          </Switch>
+        </div>
+      </HousesContext.Provider>
     </Router>
   );
 }
